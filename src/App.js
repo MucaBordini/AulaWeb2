476bcd7aef4eba8f77dd5887c350dba01e573d19
MucaBordini <mucabordini@gmail.com>
2020-08-12 10:24:43 -0300

    Aula 5

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Primeira aplicação usando creat react app</h1>
-      <TodoList titulo={"Tarefas com Propriedades"} items={['Banana', 'Abacaxi', 'Uva']}/>
+      <TodoList titulo={"Tarefas com Propriedades"} />
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 export default class TodoList extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = { items: [], novo: '' };
+    }
+
+    adicionar () {
+        this.setState((state) => {
+            return { novo: '', items: [state.novo, ...state.items] } 
+        });
+    }
+
+    handle_change (ev) {
+        this.setState({ novo: ev.target.value })
+    }
+
     render() {
         return(
             <div className="todo">
                 <h2>{this.props.titulo}</h2>
-                <input type="text" />
-                <button>Adicionar</button>
+                <input type="text" value={this.state.novo} onChange={this.handle_change.bind(this)} />
+                <button onClick={this.adicionar.bind(this)}>Adicionar</button>
                 <ul>
-                    {this.props.items.map((item, index) => 
+                    {this.state.items.map((item, index) => 
                         <li key={index}>{ item }</li>
                     )}
                 </ul>
