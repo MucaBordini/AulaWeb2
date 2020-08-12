3be715c085cf222f49c18c9fc27bac0850d0ef17
MucaBordini <mucabordini@gmail.com>
2020-08-12 12:39:45 -0300

    Aula 9 - Exemplo hooks 2

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,50 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoList.css';
 
-export default class TodoList extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = { items: [], novo: '' };
+export default function TodoList (props) {
+
+    //Substitui o contructor
+    const [items, set_items] = useState([]),
+          [novo, set_novo] = useState('');
+
+    // constructor (props) {
+    //     super(props);
+    //     this.state = { items: [], novo: '' };
+    // }
+
+    //Substitui adicionar
+    function adicionar () {
+        set_items([ {texto: novo, checked: false}, ...items]);
+        set_novo('');
+        props.set_contador(items.length + 1);
     }
 
-    adicionar () {
-        const self = this;
-        this.setState((state) => {
-            const novo_state = {
-                novo: '',
-                items: [ {texto: state.novo, checked: false}, ...state.items]
-            };
-           self.props.set_contador(novo_state.items.length);
-            return novo_state;
-        });
+    // adicionar () {
+    //     const self = this;
+    //     this.setState((state) => {
+    //         const novo_state = {
+    //             novo: '',
+    //             items: [ {texto: state.novo, checked: false}, ...state.items]
+    //         };
+    //        self.props.set_contador(novo_state.items.length);
+    //         return novo_state;
+    //     });
+    // }
+
+    //Substituido na linha 54 no onChange
+    // handle_change (ev) { 
+    //     this.setState({ novo: ev.target.value })
+    // } 
+
+    //Substitui o check
+    function check (index) {
+        set_items(items.map((item, id) => {
+                if ( id === index)
+                    return { texto: item.texto, checked: true };
+                return item;
+            })
+        );
     }
 
-    handle_change (ev) { 
-        this.setState({ novo: ev.target.value })
-    }
+    // check (index) {
+    //     this.setState((state) => {
+    //         return {
+    //             items: state.items.map((item, id) => {
+    //                 if ( id === index)
+    //                     return { texto: item.texto, checked: true };
+    //                 return item;
+    //             })
+    //         }
+    //     })
+    // }
 
-    check (index) {
-        this.setState((state) => {
-            return {
-                items: state.items.map((item, id) => {
-                    if ( id === index)
-                        return { texto: item.texto, checked: true };
-                    return item;
-                })
-            }
-        })
-    }
-    render() {
-        return(
+    //render() {
+        return (
             <div className="todo">
-                <h2>{this.props.titulo}</h2>
-                <input type="text" value={this.state.novo} onChange={this.handle_change.bind(this)} />
-                <button onClick={this.adicionar.bind(this)}>Adicionar</button>
+                <h2>{props.titulo}</h2>
+                <input type="text" value={novo} onChange={(ev) => set_novo(ev.target.value)} />
+                <button onClick={() => adicionar()}>Adicionar</button>
                 <ul>
-                    {this.state.items.map((item, index) => {
+                    {items.map((item, index) => {
                         return <li key={index}>
                             <input type="checkbox"
-                                onChange={this.check.bind(this, index)}
+                                onChange={() => check(index)}
                                 checked={item.checked}/>
                             <span className={item.checked ? "checked" : null}>{item.texto}</span>
                         </li>
@@ -54,5 +78,5 @@ export default class TodoList extends React.Component {
                 </ul>
             </div>
         );
-    }
+    //}
 }
